ced4d5c73383273049db2c8930060ed3bea920ae
dicksonnjagi <wanjiruth11@gmail.com>
2026-07-19 00:07:10 -0400

    Update: Add DOB column with MM/DD/YYYY format and update singer data

--- js/main.js
+++ js/main.js
@@ -4,26 +4,31 @@ const singerData = {
       name: "Davido",
       country: "Nigeria",
       bestSong: "Aye, Electricity",
+      dob: "11/21/1992",
     },
     {
       name: "Sauti Sol",
       country: "Kenya",
       bestSong: "Finale, Suzanna",
+      dob: "05/25/1987",
     },
     {
       name: "Ariana Grande",
       country: "USA",
       bestSong: "7 Rings, Dangerous Woman",
+      dob: "06/26/1993",
     },
     {
       name: "Nicki Minaj",
       country: "USA",
       bestSong: "Super Bass, Moment 4 Life",
+      dob: "12/08/1982",
     },
     {
       name: "Diamond Platnumz",
       country: "Tanzania",
       bestSong: "Enjoy, Kidogo",
+      dob: "10/02/1989",
     },
   ],
 };
@@ -37,6 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <td>${singer.name}</td>
       <td>${singer.country}</td>
       <td>${singer.bestSong}</td>
+      <td>${singer.dob}</td>
     `;
     tableBody.appendChild(row);
   });
